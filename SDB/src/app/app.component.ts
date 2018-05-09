6426451fc06918b02e8bda622aa3ee9c97e67d7e
Ahmet Gencoglu <ahmetgencoglu@gmail.com>
2018-05-08 22:57:00 -0400

    Recursive list

--- SDB/src/app/app.component.ts
+++ SDB/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { MenuItem } from '../menuItem';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    title = 'Schaeffler Digital Board';
+model = 2;
+
+menuItems: MenuItem[] = [
+    {label : "Automotive", link : "Hello", children: [
+        {label : "Second Item", link : "Hello"},
+        {label : "Third Item", link : "Hello"}
+    ]},
+    {label : "Industrial", link: "none", children: [
+         {label : "Second Item", link : "Hello"},
+        {label : "Third Item", link : "Hello"}
+     ]
+    },
+];
 }
--- SDB/src/app/app.module.ts
+++ SDB/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppComponent } from './app.component';
+import { AccordionComponent } from './accordion/accordion.component';
+import { ListItemComponent } from './list-item/list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccordionComponent,
+    ListItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
