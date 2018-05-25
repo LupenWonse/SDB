4de6583c3564dcaf10a0dc6f8f6e48d373c990e4
Ahmet Gencoglu <ahmetgencoglu@gmail.com>
2018-05-25 17:55:23 -0400

    Externalized Data files and added all locations

--- SDB/src/app/app.component.ts
+++ SDB/src/app/app.component.ts
@@ -2,15 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { MenuItem } from './menuItem';
 import { MouseEvent } from '@agm/core';
 import { ImageLoaderService } from './image-loader.service'
-
-interface marker {
-	lat: number;
-	lng: number;
-	label?: string;
-	draggable: boolean;
-}
-
-
+import { menuItems } from './menuItemsData'
+import { locations } from './locationsData' 
 
 @Component({
     selector: 'app-root',
@@ -33,8 +26,13 @@ export class AppComponent implements OnInit {
 
     constructor(private imageLoaderService: ImageLoaderService ) { }
     
+    private menuItems : MenuItem[];
+    locations : marker[] ;
+    
  ngOnInit() {
       this.imageLoaderService.getImage().subscribe(next => this.displayedImage = next[0]);
+     this.menuItems = menuItems;
+     this.markers = locations;
   }
     
   clickedMarker(label: string, index: number) {
@@ -137,67 +135,7 @@ export class AppComponent implements OnInit {
               elementType: 'labels.text.stroke',
               stylers: [{color: '#17263c'}]
             }
-          ]
-  
-  markers: marker[] = [
-	  {
-          label:'Fort Mill',
-		  lng: -80.9267,
-		  lat: 35.0950,
-          draggable: false,
-	  },
-	  {
-		  lat: 32.686114,
-		  lng: -79.897184,
-		  label: 'Cheraw',
-          draggable: false,
-	  },
-	  {
-		  lat: 20.787378,
-		  lng: -101.337117,
-		  label: 'Irapuato',
-          draggable: false,
-	  }
-  ]
-    
-menuItems: MenuItem[] = [
-    {id: 1, level: 0, label : "Automotive", icon:"fa-car", charts : [], children: [
-        {id: 2, icon: "fa-check-circle", level: 1, label : "Quality", charts : [], children: [
-            {id: 2, icon: "",level: 2, label : "Customer Complaints", charts : ['1_Chart 1.jpg', '2_Chart 2.jpg'],children : []},
-            {id: 2, icon: "",level: 2, label : "Scrap", charts : ['3_Chart 1.jpg', '4_Chart 3.jpg'], children : []},
-            {id: 2, icon: "",level: 2, label : "Material Consumption Variance", charts : ['8_Chart 6.jpg','7_Chart 2.jpg'],children : []},
-            {id: 2, icon: "",level: 2, label : "External Sorting Cost", charts : [],children : []},
-            {id: 2, icon: "",level: 2, label : "Supplier Complaints", charts : [],children : []},
-        ]},
-        {id: 3, icon: "fa-money" ,level: 1, label : "Cost", charts : [],children:[
-            {id: 2, icon: "",level: 2, label : "Inventory", charts : ['5_Chart 1.jpg','6_Chart 2.jpg'],children : []},
-             {id: 4,icon: "", level: 2, label : "Direct Personnel Deviation", charts : ['9_Chart 1.jpg'],children : []},
-             {id: 4,icon: "", level: 2, label : "Value Added", charts : ['12_Chart 3.jpg','13_Chart 4.jpg'],children : []},
-        ]},
-        {id: 3, icon: "fa-truck" ,level: 1, label : "Delivery", charts : [],children:[
-             {id: 4,icon: "", level: 2, label : "Delivery", charts : ['10_Chart 4.jpg','11_Chart 5.jpg'],children : []},
-        ]}
-    ]},
-    {id: 1, level: 0, label : "Industrial", icon:"fa-industry", charts : [], children: [
-        {id: 2, icon: "fa-check-circle", level: 1, label : "Quality", charts : [], children: [
-            {id: 2, icon: "",level: 2, label : "Customer Complaints", charts : ['1_Chart1.jpg', '2_Chart 2.jpg'],children : []},
-            {id: 2, icon: "",level: 2, label : "Scrap", charts : ['3_Chart 1.jpg', '4_Chart 3.jpg'], children : []},
-            {id: 2, icon: "",level: 2, label : "Material Consumption Variance", charts : ['8_Chart 6.jpg','7_Chart 2.jpg'],children : []},
-            {id: 2, icon: "",level: 2, label : "External Sorting Cost", charts : [],children : []},
-            {id: 2, icon: "",level: 2, label : "Supplier Complaints", charts : [],children : []},
-        ]},
-        {id: 3, icon: "fa-money" ,level: 1, label : "Cost", charts : [],children:[
-            {id: 2, icon: "",level: 2, label : "Inventory", charts : ['5_Chart 1.jpg','6_Chart 2.jpg'],children : []},
-             {id: 4,icon: "", level: 2, label : "Direct Personnel Deviation", charts : ['9_Chart 1.jpg'],children : []},
-             {id: 4,icon: "", level: 2, label : "Value Added", charts : ['12_Chart 3.jpg','13_Chart 4.jpg'],children : []},
-        ]},
-        {id: 3, icon: "fa-truck" ,level: 1, label : "Delivery", charts : [],children:[
-             {id: 4,icon: "", level: 2, label : "Delivery", charts : ['10_Chart 4.jpg','11_Chart 5.jpg'],children : []},
-        ]}
-    ]}
-];
-    
-    
+          ]   
 }
 
 
---
+++ SDB/src/app/locationsData.ts
@@ -0,0 +1,99 @@
+interface marker {
+	lat: number;
+	lng: number;
+	label?: string;
+	draggable: boolean;
+}
+
+export const locations : marker[] = [
+      {
+          label: 'Stratford AT',
+          lat: 43.369671,
+		  lng: -80.954288,
+          draggable: false,
+	  },
+      {
+          label: 'Stratssssford AS',
+          lat: 43.356231,
+		  lng: -81.008342,
+          draggable: false,
+	  },
+      {
+          label: 'Wooster',
+          lat: 40.836994,
+		  lng: -81.907797,
+          draggable: false,
+	  },
+      {
+          label: 'Joplin',
+          lat: 37.046160,
+		  lng: -94.468736,
+          draggable: false,
+	  },
+      {
+          label: 'Danbury',
+          lat: 41.378997,
+		  lng: -73.471377,
+          draggable: false,
+	  },
+      {
+          label: 'Fort Mill 1',
+          lat: 35.091573,
+		  lng: -80.927140,
+          draggable: false,
+	  },
+      {
+          label: 'Fort Mill 2',
+          lat: 35.096878,
+		  lng: -80.927884,
+          draggable: false,
+	  },
+      {
+          label: 'Cheraw 1 & 5',
+          lat: 34.685248,
+		  lng: -78.895565,
+          draggable: false,
+	  },
+      {
+          label: 'Cheraw 2',
+          lat: 34.696710,
+		  lng: -79.930007,
+          draggable: false,
+	  },
+      {
+          label: 'Spartanburg',
+          lat: 34.981336,
+		  lng: -81.995681,
+          draggable: false,
+	  },
+      {
+          label: 'Puebla 1',
+          lat: 19.160926,
+		  lng: -98.380319,
+          draggable: false,
+	  },
+      {
+          label: 'Puebla 2',
+          lat: 19.161350,
+		  lng: -98.379021,
+          draggable: false,
+	  },
+      {
+          label: 'Irapuato',
+          lat: 20.787781,
+		  lng: -101.337021,
+          draggable: false,
+	  },
+      {
+          label: 'Soracaba 1',
+          lat: -23.438080,
+		  lng: -47.414647,
+          draggable: false,
+	  },
+      {
+          label: 'Soracaba 2',
+          lat: -23.438621,
+		  lng: -47.416026,
+          draggable: false,
+	  }
+  ]
